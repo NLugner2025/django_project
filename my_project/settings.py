7d73cbb53f671bd5a729f4ff8b16cba9e7bb92dd
Nlugner-GitPod <nils@lugner.uk>
2025-03-17 08:35:06 +0000

    commited again

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mk&27ng#9#w+^k-4)6g50nva@6bmf)=&*%b$pp$3tb#l@3o@vp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com','127.0.0.1','
+]
 
 # Application definition
 
